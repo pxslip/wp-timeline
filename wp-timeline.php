7efbbbac90b08021ead8a37bcc2382b7fa9fca08
Will Kruse <wskruse@gmail.com>
2013-12-04 12:24:46 -0500

    added plugin function file

---
+++ wp-timeline.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Wordpress Timeline
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: A wordpress plugin for creating timelines quickly and easily using wordpress functionality and TimelineJS
+ * Version: 0.1A
+ * Author: Will Kruse
+ * Author URI: http://wskruse.github.io
+ * License: MIT
+ */
